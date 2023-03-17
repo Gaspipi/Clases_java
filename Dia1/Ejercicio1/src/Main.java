bbdb747b19b23cd0d59fb31e9090eb443f9a1509
Gaspipi <47346700+Gaspipi@users.noreply.github.com>
2023-03-17 18:04:52 -0300

    asd

--- Dia1/Ejercicio1/src/Main.java
+++ Dia1/Ejercicio1/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+               int numEntero = 400000; //Int -> Integer / entero
+                Short numChico = 400; //
+                Long numGrandes = 5000234L;
+                String nombre = "Roman"; // Cadena de caracteres
+                Character letra = 'C'; // Caracter
+                Boolean bool = Boolean.TRUE; //V - F
+                Float flotante = 5.7346346373474373743573457f;
+                Double NumConComas = 4.567456454;
+
+
+             System.out.println(numChico + " * " + NumConComas + "=" + numChico*NumConComas);
+
+
+
+
+
+
+
+
     }
 }
